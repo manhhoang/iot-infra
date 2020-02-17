38d80c719d04af7537ce759ba8e7f094b90a6aee
Manh Hoang Ha <manhhoang.ha@qrious.co.nz>
2020-02-17 17:17:35 +1300

    Add bigquery.

--- apps/ingest/README.md
+++ apps/ingest/README.md
@@ -6,5 +6,8 @@
 brew install librdkafka pkg-config
 
 go get -u gopkg.in/confluentinc/confluent-kafka-go.v1/kafka
+go get -u cloud.google.com/go/bigquery
+
+export GOOGLE_APPLICATION_CREDENTIALS="/home/user/Downloads/[FILE_NAME].json"
 
 ```
--- apps/ingest/main.go
+++ apps/ingest/main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"fmt"
+	"google.golang.org/api/iterator"
 	"gopkg.in/confluentinc/confluent-kafka-go.v1/kafka"
+	"cloud.google.com/go/bigquery"
+	"io"
 )
 
 func main() {
@@ -29,4 +32,67 @@ func main() {
 	}
 
 	c.Close()
+}
+
+func insertData() {
+	ctx := context.Background()
+	client, err := bigquery.NewClient(ctx, projectID)
+	if err != nil {
+		// TODO: Handle error.
+	}
+
+	q := client.Query(`
+    SELECT year, SUM(number) as num
+    FROM ` + "`bigquery-public-data.usa_names.usa_1910_2013`" + `
+    WHERE name = "William"
+    GROUP BY year
+    ORDER BY year
+`)
+	it, err := q.Read(ctx)
+	if err != nil {
+		// TODO: Handle error.
+	}
+}
+
+// queryBasic demonstrates issuing a query and reading results.
+func queryBasic(w io.Writer, projectID string) error {
+	// projectID := "my-project-id"
+	ctx := context.Background()
+	client, err := bigquery.NewClient(ctx, projectID)
+	if err != nil {
+		return fmt.Errorf("bigquery.NewClient: %v", err)
+	}
+	defer client.Close()
+
+	q := client.Query(
+		"SELECT name FROM `bigquery-public-data.usa_names.usa_1910_2013` " +
+			"WHERE state = \"TX\" " +
+			"LIMIT 100")
+	// Location must match that of the dataset(s) referenced in the query.
+	q.Location = "US"
+	// Run the query and print results when the query job is completed.
+	job, err := q.Run(ctx)
+	if err != nil {
+		return err
+	}
+	status, err := job.Wait(ctx)
+	if err != nil {
+		return err
+	}
+	if err := status.Err(); err != nil {
+		return err
+	}
+	it, err := job.Read(ctx)
+	for {
+		var row []bigquery.Value
+		err := it.Next(&row)
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			return err
+		}
+		fmt.Fprintln(w, row)
+	}
+	return nil
 }
